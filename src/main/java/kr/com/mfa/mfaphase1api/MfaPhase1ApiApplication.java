a7fc01da297bca1d3da8c74f38e5af5f1bb49f1e
Sovannakk <sovannak.khengg@gmail.com>
2025-08-25 17:14:50 +0700

    init project

---
+++ src/main/java/kr/com/mfa/mfaphase1api/MfaPhase1ApiApplication.java
@@ -0,0 +1,13 @@
+package kr.com.mfa.mfaphase1api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfaPhase1ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MfaPhase1ApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/com/mfa/mfaphase1api/MfaPhase1ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.com.mfa.mfaphase1api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MfaPhase1ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
